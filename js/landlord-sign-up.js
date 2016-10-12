36c71991786127cd7828f417630749c433819687
Alexandra Persea <perseaalexandra@gmail.com>
2016-10-13 00:50:47 +0300

    Landlord Sign up page and form created.

---
+++ js/landlord-sign-up.js
@@ -0,0 +1,26 @@
+// Landlord sign-up form javascript
+function LandlordSignUp() {
+
+  $( "#landlord-sign-up" ).submit(function( event ) {
+
+    // Stop form from submitting
+    event.preventDefault();
+
+    // Get values from sign-up form
+    var $name = $('#land-first-name').val() + " " + $('#land-last-name').val();
+    var $email = $('#land-email').val();
+    var $managed_units = $('#managed-units-count').val();
+    var $password = $('#land-password').val();
+
+    // Send the data using post
+    $.ajax({
+      type: "POST",
+      url: "http://app.plexmate.com/users",
+      data: { user: { name: $name, email: $email, password: $password } },
+    });
+
+    // Submit message
+    $('.sign-up-container').html("<h2>Thank you!</h2><p>An account has been created.</p>");
+
+  });
+}
