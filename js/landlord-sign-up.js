926e201bc9f7c5b2d466622c1f00f0434ec74b2a
Alexandra Persea <perseaalexandra@gmail.com>
2016-11-22 09:18:14 +0200

    Validations added to landlord signup form.

--- js/landlord-sign-up.js
+++ js/landlord-sign-up.js
@@ -11,17 +11,30 @@ function LandlordSignUp() {
     var $email = $('#land-email').val();
     var $managed_units = $('#managed-units-count').val();
     var $password = $('#land-password').val();
+    var $password_confirmation = $('#password-confirm').val();
     var $landlord = true;
 
-    // Send the data using post
+    if($password != $password_confirmation) {
+      // Append errors.
+      $('.form-errors').html('Passwords must match!');
+    }
+    else {
     $.ajax({
       type: "POST",
       url: "http://app.plexmate.com/api/signups",
+      headers: {
+      'api_application_key':'CVvMQ4ZU5FCRuSMzeM7c'
+      },
       data: { user: { name: $name, email: $email, password: $password , landlord: $landlord} },
+      success: function(response) {
+        // Submit message.
+        $('.sign-up-container').html('<h2>Thank you!</h2><p>Your account has been created.</p><a class="btn btn-filled btn-lg mb32 mt-xs-40" href="http://app.plexmate.com/sign_in">Login</a>');
+      },
+      error: function(XMLHttpRequest, textStatus) {
+        // Append errors.
+        $('.form-errors').html(XMLHttpRequest.responseText);
+      }
     });
-
-    // Submit message
-    $('.sign-up-container').html("<h2>Thank you!</h2><p>An account has been created.</p>");
-
+  }
   });
 }
